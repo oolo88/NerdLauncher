5e1f32aa72d14c6dc3b10061daf55a5d889e782a
C. Chang <conradc88@yahoo.com>
2015-05-08 11:53:22 +0800

    20150508

--- app/src/main/java/com/android/bignerdranch/nerdlauncher/NerdLauncherActivity.java
+++ app/src/main/java/com/android/bignerdranch/nerdlauncher/NerdLauncherActivity.java
@@ -1,38 +1,16 @@
 package com.android.bignerdranch.nerdlauncher;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class NerdLauncherActivity extends ActionBarActivity {
+public class NerdLauncherActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nerd_launcher);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_nerd_launcher, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public Fragment createFragment() {
+        return new NerdLauncherFragment();
     }
 }
---
+++ app/src/main/java/com/android/bignerdranch/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,31 @@
+package com.android.bignerdranch.nerdlauncher;
+
+import android.app.ListFragment;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * Created by ConradC on 8/5/15.
+ */
+public class NerdLauncherFragment extends ListFragment {
+    private static final String TAG = "NerdLauncherFragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        Log.i(TAG, "I've found " + activities.size() + " activities."):
+    }
+
+}
